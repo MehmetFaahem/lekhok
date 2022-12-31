0754647172620dedc105e0955f421b6865a1d050
mehmetfaahem <mehmetfaahem@gmail.com>
2023-01-01 01:11:22 +0600

    fixed

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Header />
+        <section className="flex">
+          <div className="flex place-items-center">
+            <div>
+              <h1 className="text-2xl">Lekhok</h1>
+            </div>
+            <div className="flex ml-96 space-x-3">
+              <Link href="/login">
+                <button className="p-3 bg-slate-800 text-white rounded-xl">
+                  Log In
+                </button>
+              </Link>
+              <Link href="/register">
+                <button className="p-3 bg-slate-800 text-white rounded-xl">
+                  Register
+                </button>
+              </Link>
+            </div>
+          </div>
+        </section>
         <Categories />
       </main>
     </>
